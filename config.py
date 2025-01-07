03d72bb0c13103af56b13d368fc6ea4ecb5a2dc1
BiK <0713chosen@gmail.com>
2025-01-07 14:22:43 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-FETV_T2V_model=['cogvideo','text2video-zero','modelscope-t2v','zeroscope']
-# FETV_T2V_model=['text2video-zero','modelscope-t2v','zeroscope']
-LGVQ_T2V_model=['Gen-2','Hotshot-XL','Text2Video-Zero','Tune-A-Video','VideoCrafter','video_fusion']
-# FETV_path='/home/user/Documents/vqadata/FETV'
-FETV_vid_path = [f'/home/user/Documents/vqadata/FETV/{mdl}/videos' for mdl in FETV_T2V_model]
-
